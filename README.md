d6c9b86be783a7da7f11cabf2bae332b98e76059
Adam Syed <suedadam@gmail.com>
2018-02-25 00:12:46 -0800

    Update for people to view

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ScreenLock
-Figure out how CGSession works and make my own version of the screen switcher
+
+This is a super simple link to replicate the functionality of "CGSession -suspend". This was simply to see if I could for fun. If you're from 42 then just go to the lab, don't abuse it to get lab hours :( 
--- lock.c
+++ lock.c
@@ -6,17 +6,30 @@
 /*   By: asyed <asyed@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/24 20:52:40 by asyed             #+#    #+#             */
-/*   Updated: 2018/02/24 20:54:59 by asyed            ###   ########.fr       */
+/*   Updated: 2018/02/25 00:12:30 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dlfcn.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int	main(void)
 {
 	void	*handle;
 	void	(*func)(void);
+	char	*name;
 
+	if (!(name = calloc(1024, sizeof(char *))))
+		return (-1);
+	if (gethostname(name, 1024) == -1)
+		return (-1);
+	if (strstr(name, "42.us.org"))
+		return (-1);
+	free(name);
+	name = NULL;
 	handle = dlopen("/System/Library/CoreServices/Menu Extras/User.menu/Contents/MacOS/User", RTLD_LAZY);
 	if (!handle)
 		return (-1);
